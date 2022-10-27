3dd5260e1dd775264cbba1ed1704c25c3915ca3c
Jior <JamesIoriattiII2023@u.northwestern.edu>
2022-10-27 18:26:02 -0500

    Added in time-checking functionality using data-attributes

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ setInterval(currentDate, 1000);
 dateContainer.style.fontSize = "32px";
 dateContainer.style.fontWeight = "bold";
 
+
+
+
+
 const buttonEl = document.getElementById("btn");
 
 function generateSchedule(){
@@ -29,23 +33,58 @@ function generateSchedule(){
         if((i+7)>12){
             schedRow.textContent = (i-5) + " pm";
         }
+        //Data attribute will be used for the isPastHour function
+        schedRow.setAttribute("data-hour", i+7);
         schedRow.setAttribute("class", "row");
         scheduleContainer.appendChild(schedRow);
-        //Couldn't get the schedule-row class styles to apply from the CSS file, so I'm applying styles within the function itself
-        // schedRow.style.margin = "5em";
     };
+    const currentTime = moment().hour();
+    console.log(currentTime);
+    const schedRow = document.querySelectorAll(".row");
+    console.log($('.row').data().hour);
+    //YES THIS WORKED!
+    for(let i=0; i<13; i++){
+        dataNum = $('.row').data().hour;
+        console.log(dataNum);
+        if(currentTime>dataNum){
+            schedRow[i].classList.add("past");
+            console.log("past");
+        };
+        if(currentTime===dataNum){
+            schedRow[i].classList.add("present");            console.log("present");
+        };
+        if(currentTime<dataNum){
+            schedRow[i].classList.add("future");
+            console.log("future");
+        };
+    };
+
     buttonEl.setAttribute("disabled","disabled");
     buttonEl.style.pointerEvents = "none"
     buttonEl.style.filter = "grayscale(100%)";
     const resetButton = document.createElement("button");
+    resetButton.setAttribute("class", "reset");
     resetButton.textContent = "RESET";
     headerEl.appendChild(resetButton);
     const htmlEl = document.querySelector("html");
     htmlEl.style.scrollBehavior="smooth";
+    
+    const resetButtonEl = document.querySelector(".reset");
+//Creating a function to reset the state of the webpage if the user decides to clear/start over.
+//May add in local storage to create a revert/history button.
+    function reset(){
+        for(let i=0; i<=12;i++){
+            schedRow[i].remove();
+        };
+        resetButtonEl.remove();
+        buttonEl.removeAttribute("disabled", "disabled");
+        buttonEl.style.filter = "none";
+        buttonEl.style.pointerEvents = "initial";
+    };
+    resetButtonEl.addEventListener("click", reset);
 };
 
 
+buttonEl.addEventListener("click", generateSchedule);
 
-
-buttonEl.addEventListener("click", generateSchedule)
 
