d63266dedd485cf9e73978a6e969579e1995486e
Jior <JamesIoriattiII2023@u.northwestern.edu>
2022-10-28 08:53:15 -0500

    Fixed the hour-checking functionality

--- script.js
+++ script.js
@@ -15,49 +15,62 @@ setInterval(currentDate, 1000);
 dateContainer.style.fontSize = "32px";
 dateContainer.style.fontWeight = "bold";
 
+//Declaring a function to utilize the jquery fadeIn() and fadeOut() methods to manipulate elements come one screen when scrolling.
+function fadeElements(){
 
-
+};
 
 
 const buttonEl = document.getElementById("btn");
 
 function generateSchedule(){
-    for(let i=0; i<=12;i++){
+    const currentHour = moment().hour();
+    console.log(currentHour);
+    for(let i=0; i<=15;i++){
         const schedRow = document.createElement('div');
-        if((i+7)<=12){
-            schedRow.textContent = (i+7) + " am";
+        if((i+5)<=12){
+            schedRow.textContent = (i+5) + " am";
         }
-        if((i+7)===12){
-            schedRow.textContent = (i+7) + " am (noon)";
+        if((i+5)===12){
+            schedRow.textContent = (i+5) + " am (Noon)";
         }
-        if((i+7)>12){
-            schedRow.textContent = (i-5) + " pm";
+        if((i+5)>12){
+            schedRow.textContent = (i-3) + " pm";
         }
-        //Data attribute will be used for the isPastHour function
-        schedRow.setAttribute("data-hour", i+7);
+        //Giving each div row a data attribute refering to which hour it blongs to.
+        schedRow.setAttribute("data-hour", i+5);
         schedRow.setAttribute("class", "row");
         scheduleContainer.appendChild(schedRow);
     };
-    const currentTime = moment().hour();
-    console.log(currentTime);
-    const schedRow = document.querySelectorAll(".row");
+    //Reassigning the shedRow variable to target all newly generated div rows.
+    schedRow = document.querySelectorAll(".row");
     console.log($('.row').data().hour);
-    //YES THIS WORKED!
-    for(let i=0; i<13; i++){
-        dataNum = $('.row').data().hour;
-        console.log(dataNum);
-        if(currentTime>dataNum){
+    //YES THIS WORKED! Had a lot of trouble trying to access the data- attribute, but noticed in objects of the schedRow array that the key:pair nomenclature was dataset:hour, not data:hour!
+    console.log(schedRow);
+    // debugger;
+    for(let i=0; i<16; i++){
+        console.log(schedRow[i].dataset.hour);
+        if(currentHour>schedRow[i].dataset.hour){
             schedRow[i].classList.add("past");
             console.log("past");
         };
-        if(currentTime===dataNum){
-            schedRow[i].classList.add("present");            console.log("present");
+        if(currentHour==schedRow[i].dataset.hour){
+            schedRow[i].classList.add("present");
+            console.log("present");
         };
-        if(currentTime<dataNum){
+        if(currentHour<schedRow[i].dataset.hour){
             schedRow[i].classList.add("future");
             console.log("future");
         };
     };
+    //Creating other schedule elements, the text fields and the save buttons.
+    for(let i=0; i<=15;i++){
+        const saveButton = document.createElement('button');
+        schedRow[i].appendChild(saveButton);
+        saveButton.textContent = "Save";
+        saveButton.setAttribute("class", "saveBtn")
+    };
+
 
     buttonEl.setAttribute("disabled","disabled");
     buttonEl.style.pointerEvents = "none"
@@ -67,13 +80,14 @@ function generateSchedule(){
     resetButton.textContent = "RESET";
     headerEl.appendChild(resetButton);
     const htmlEl = document.querySelector("html");
+    //Adding a smooth scroll property to the entire HTML, planning on having the window scroll down to the schedule when the GET STARTED button is clicked.
     htmlEl.style.scrollBehavior="smooth";
     
     const resetButtonEl = document.querySelector(".reset");
 //Creating a function to reset the state of the webpage if the user decides to clear/start over.
 //May add in local storage to create a revert/history button.
     function reset(){
-        for(let i=0; i<=12;i++){
+        for(let i=0; i<=15;i++){
             schedRow[i].remove();
         };
         resetButtonEl.remove();
